e26c496ec6d7170352be8ab67aec2a1e27872849
Tyler Creller <tylercreller@gmail.com>
2019-11-29 19:03:30 -0500

    Error Handler and cleanup

---
+++ middleware/error.js
@@ -0,0 +1,33 @@
+const ErrorResponse = require('../utils/errorResponse');
+
+const errorHandler = (err, req, res, next) => {
+	let error = { ...err };
+	error.message = err.message;
+	// Log to console for dev
+	console.log(err);
+
+	// Mongoose bad ObjectID
+	if (error.name === 'CastError') {
+		const message = `Resource not found with id of ${error.value}`;
+		error = new ErrorResponse(message, 404);
+	}
+
+	// Mongoose duplicate key
+	if (error.code === 11000) {
+		const message = 'Duplicate field value entered';
+		error = new ErrorResponse(message, 400);
+	}
+
+	// Mongoos validation error
+	if (error.name === 'ValidationError') {
+		const message = Object.values(err.errors).map(val => val.message);
+		error = new ErrorResponse(message, 400);
+	}
+
+	res.status(error.statusCode || 500).json({
+		success: false,
+		error: error.message || 'Server Error'
+	});
+};
+
+module.exports = errorHandler;
