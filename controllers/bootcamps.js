e510d7b702c1ef21f421b988cf7a34fdfc4a5978
Tyler Creller <tylercreller@gmail.com>
2019-12-09 17:55:40 -0500

    Page, skip, limit, sort

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,11 +8,57 @@ const asyncHandler = require('../middleware/async');
 // @access	Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
 	let query;
-	let queryStr = JSON.stringify(req.query);
+
+	const reqQuery = { ...req.query };
+
+	// Fields to exclude
+	const removeFields = ['select', 'sort', 'page', 'limit'];
+
+	removeFields.forEach(param => delete reqQuery[param]);
+
+	let queryStr = JSON.stringify(reqQuery);
+
 	queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 	query = Bootcamp.find(JSON.parse(queryStr));
+
+	// Select fields
+	if (req.query.select) {
+		query.select(req.query.select.split(',').join(' '));
+	}
+
+	if (req.query.sort) {
+		query.sort(req.query.sort.split(',').join(' '));
+	} else {
+		query.sort('-createdAt');
+	}
+
+	// Pagination
+	const page = parseInt(req.query.page, 10) || 1;
+	const limit = parseInt(req.query.limit, 10) || 25;
+	const startIndex = (page - 1) * limit;
+	const endIndex = page * limit;
+	const total = await Bootcamp.countDocuments();
+	query.skip(startIndex).limit(limit);
+
 	const bootcamps = await query;
-	res.status(200).json({ success: true, data: bootcamps });
+
+	// Pagination result
+	const pagination = {};
+	if (endIndex < total) {
+		pagination.next = {
+			page: page + 1,
+			limit
+		};
+	}
+
+	if (startIndex > 0) {
+		pagination.prev = {
+			page: page - 1,
+			limit
+		};
+	}
+
+	res.status(200).json({ success: true, pagination, data: bootcamps });
 });
 
 // @desc		Get Single bootcamp
